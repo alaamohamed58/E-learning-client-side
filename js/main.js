12b9d11760eb422b5978803da40c321d9a183843
Mohammed Turky <mturky@cne.grp>
2025-06-23 14:42:50 +0300

    activating enroll course, hide create course based on user role, adding logout in profile page

--- js/main.js
+++ js/main.js
@@ -1,195 +1,187 @@
 (function ($) {
-    "use strict";
+  "use strict";
 
-    // Spinner
-    var spinner = function () {
-        setTimeout(function () {
-            if ($('#spinner').length > 0) {
-                $('#spinner').removeClass('show');
-            }
-        }, 1);
-    };
-    spinner();
-    
-    
-    // Initiate the wowjs
-    new WOW().init();
+  // Spinner
+  var spinner = function () {
+    setTimeout(function () {
+      if ($("#spinner").length > 0) {
+        $("#spinner").removeClass("show");
+      }
+    }, 1);
+  };
+  spinner();
 
+  // Initiate the wowjs
+  new WOW().init();
 
-    // Sticky Navbar
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 300) {
-            $('.sticky-top').css('top', '0px');
-        } else {
-            $('.sticky-top').css('top', '-100px');
+  // Sticky Navbar
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      $(".sticky-top").css("top", "0px");
+    } else {
+      $(".sticky-top").css("top", "-100px");
+    }
+  });
+
+  // Dropdown on mouse hover
+  const $dropdown = $(".dropdown");
+  const $dropdownToggle = $(".dropdown-toggle");
+  const $dropdownMenu = $(".dropdown-menu");
+  const showClass = "show";
+
+  $(window).on("load resize", function () {
+    if (this.matchMedia("(min-width: 992px)").matches) {
+      $dropdown.hover(
+        function () {
+          const $this = $(this);
+          $this.addClass(showClass);
+          $this.find($dropdownToggle).attr("aria-expanded", "true");
+          $this.find($dropdownMenu).addClass(showClass);
+        },
+        function () {
+          const $this = $(this);
+          $this.removeClass(showClass);
+          $this.find($dropdownToggle).attr("aria-expanded", "false");
+          $this.find($dropdownMenu).removeClass(showClass);
         }
-    });
-    
-    
-    // Dropdown on mouse hover
-    const $dropdown = $(".dropdown");
-    const $dropdownToggle = $(".dropdown-toggle");
-    const $dropdownMenu = $(".dropdown-menu");
-    const showClass = "show";
-    
-    $(window).on("load resize", function() {
-        if (this.matchMedia("(min-width: 992px)").matches) {
-            $dropdown.hover(
-            function() {
-                const $this = $(this);
-                $this.addClass(showClass);
-                $this.find($dropdownToggle).attr("aria-expanded", "true");
-                $this.find($dropdownMenu).addClass(showClass);
-            },
-            function() {
-                const $this = $(this);
-                $this.removeClass(showClass);
-                $this.find($dropdownToggle).attr("aria-expanded", "false");
-                $this.find($dropdownMenu).removeClass(showClass);
-            }
-            );
-        } else {
-            $dropdown.off("mouseenter mouseleave");
-        }
-    });
-    
-    
-    // Back to top button
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 300) {
-            $('.back-to-top').fadeIn('slow');
-        } else {
-            $('.back-to-top').fadeOut('slow');
-        }
-    });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
-    });
+      );
+    } else {
+      $dropdown.off("mouseenter mouseleave");
+    }
+  });
 
+  // Back to top button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      $(".back-to-top").fadeIn("slow");
+    } else {
+      $(".back-to-top").fadeOut("slow");
+    }
+  });
+  $(".back-to-top").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 1500, "easeInOutExpo");
+    return false;
+  });
 
-    // Header carousel
-    $(".header-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1500,
+  // Header carousel
+  $(".header-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1500,
+    items: 1,
+    dots: false,
+    loop: true,
+    nav: true,
+    navText: [
+      '<i class="bi bi-chevron-left"></i>',
+      '<i class="bi bi-chevron-right"></i>',
+    ],
+  });
+
+  // Testimonials carousel
+  $(".testimonial-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1000,
+    center: true,
+    margin: 24,
+    dots: true,
+    loop: true,
+    nav: false,
+    responsive: {
+      0: {
         items: 1,
-        dots: false,
-        loop: true,
-        nav : true,
-        navText : [
-            '<i class="bi bi-chevron-left"></i>',
-            '<i class="bi bi-chevron-right"></i>'
-        ]
-    });
-
-
-    // Testimonials carousel
-    $(".testimonial-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 1000,
-        center: true,
-        margin: 24,
-        dots: true,
-        loop: true,
-        nav : false,
-        responsive: {
-            0:{
-                items:1
-            },
-            768:{
-                items:2
-            },
-            992:{
-                items:3
-            }
-        }
-    });
-    
+      },
+      768: {
+        items: 2,
+      },
+      992: {
+        items: 3,
+      },
+    },
+  });
 })(jQuery);
 
 async function redirectToLogin(event) {
-        event.preventDefault(); // Stop default form submission
+  event.preventDefault(); // Stop default form submission
 
-        // Gather form data
-        const firstName = document.getElementById("first-name").value.trim();
-        const lastName = document.getElementById("last-name").value.trim();
-        const gender = document.getElementById("gender").value;
-        const role = Number(document.getElementById("role").value);
-        const age = document.getElementById("age").value.trim();
-        const email = document.getElementById("email").value.trim();
-        const password = document.getElementById("password").value;
+  // Gather form data
+  const firstName = document.getElementById("first-name").value.trim();
+  const lastName = document.getElementById("last-name").value.trim();
+  const gender = document.getElementById("gender").value;
+  const role = Number(document.getElementById("role").value);
+  const age = document.getElementById("age").value.trim();
+  const email = document.getElementById("email").value.trim();
+  const password = document.getElementById("password").value;
 
-        const payload = {
-          first_name: firstName,
-          last_name: lastName,
-          email,
-          role,
-          password,
-        };
+  const payload = {
+    first_name: firstName,
+    last_name: lastName,
+    email,
+    role,
+    password,
+  };
 
-        try {
-          console.log(JSON.stringify(payload));
-          const response = await fetch(
-            "http://localhost:3000/api/v1/auth/register",
-            {
-              method: "POST",
-              headers: {
-                "Content-Type": "application/json",
-              },
-              body: JSON.stringify(payload),
-            }
-          );
+  try {
+    console.log(JSON.stringify(payload));
+    const response = await fetch("http://localhost:3000/api/v1/auth/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+    });
 
-          const data = await response.json();
+    const data = await response.json();
 
-          if (response.ok) {
-            alert("Signup successful! Redirecting to login page...");
-            window.location.href = "login.html";
-          } else {
-            alert("Signup failed: " + (data.message || "Unknown error"));
-          }
-        } catch (error) {
-          console.error("Error:", error);
-          alert("An error occurred while registering.");
-        }
-      }
-
-
-
-       async function redirectToProfile(event) {
-    event.preventDefault(); // Prevent default form submission
-
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-
-    try {
-        const response = await fetch("http://localhost:3000/api/v1/auth/login", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({ email, password })
-        });
-
-        console.log(response)
-        if (response.status === 200) {
-            
-            const data = await response.json();
-            const token = data.accessToken
-            console.log("Login successful! Token:", token);
-
-            localStorage.setItem("authToken", token);
-             window.location.href = "profile.html"; // Redirect to Profile page
-        } else {
-            const result = await response.json();
-            alert(result.message || "Login failed. Please try again.");
-        }
-    } catch (error) {
-        alert("An error occurred: " + error.message);
+    if (response.ok) {
+      alert("Signup successful! Redirecting to login page...");
+      window.location.href = "login.html";
+    } else {
+      alert("Signup failed: " + (data.message || "Unknown error"));
     }
+  } catch (error) {
+    console.error("Error:", error);
+    alert("An error occurred while registering.");
+  }
+}
+
+async function redirectToProfile(event) {
+  event.preventDefault(); // Prevent default form submission
+
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  try {
+    const response = await fetch("http://localhost:3000/api/v1/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    });
+
+    console.log(response);
+    if (response.status === 200) {
+      const data = await response.json();
+      console.log(data);
+      const token = data.accessToken;
+      const role = data.role;
+
+      console.log("Login successful! Token:", token);
+
+      localStorage.setItem("authToken", token);
+      localStorage.setItem("role", role);
+
+      window.location.href = "profile.html"; // Redirect to Profile page
+    } else {
+      const result = await response.json();
+      alert(result.message || "Login failed. Please try again.");
+    }
+  } catch (error) {
+    alert("An error occurred: " + error.message);
+  }
 }
 
 function logout() {
-    localStorage.removeItem("authToken");
-    window.location.href = "index.html";
-  }
+  localStorage.removeItem("authToken");
+  window.location.href = "index.html";
+}
